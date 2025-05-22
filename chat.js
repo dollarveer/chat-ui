091a3e91b77fe578ca396438225cbc3cb4793c53
dollarveer <85345503+dollarveer@users.noreply.github.com>
2025-05-22 22:38:27 +0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -405,11 +405,13 @@ function populateChatBubbles(chatId, newMsgs = 0) {
 				const replyAlias = renderAlias(replyMsg.sender_hash, userhash, aliasMap, chatType);
 				if (replyAlias === false && chatType === "Group") return;
 
-				const replyText = replyMsg.message_content ? replyMsg.message_content : '[Original message]';;
+				const replyText = replyMsg.message_content ? replyMsg.message_content : '[Original message]';
 				replyDiv.setAttribute("onclick", `scrollToOriginalMessage(${replyMsg.messageId})`);
 				replyDiv.setAttribute("style", `font-size:13px;color:#ccc;margin-bottom:5px;margin-right: 20px; border-left:2px solid #aaa;padding-left:8px;`);
 				replyDiv.innerHTML = ` ↪ <strong>${replyAlias}:</strong>${replyText}</br> ${decryptMessage(replyText, chatPrint)}`;
 				bubble.appendChild(replyDiv);
+
+				alert(replyMsg);
 			}
 		}
 
