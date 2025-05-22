c6dff85f1610e5392acf78f037bc379ecd344650
dollarveer <85345503+dollarveer@users.noreply.github.com>
2025-05-22 21:56:29 +0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -399,7 +399,7 @@ function populateChatBubbles(chatId, newMsgs = 0) {
 		// Reply preview
 		if (msg.reply_to) {
 			const replyDiv = document.createElement('div');
-			const replyMsg = msgs.find(m => JSON.parse(decryptMessage(m, chatMessages[chatId].chatPrint)).messageId === msg.reply_to);
+			let replyMsg = msgs.find(m => JSON.parse(decryptMessage(m, chatMessages[chatId].chatPrint)).messageId === msg.reply_to);
 			replyMsg = decryptMessage(replyMsg, chatPrint);
 			if (replyMsg) {
 				const replyAlias = renderAlias(replyMsg.sender_hash, userhash, aliasMap, chatType);
