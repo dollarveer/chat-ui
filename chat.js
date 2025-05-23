e6c87b58ddd4238a995f2632c132c0cac7cd7571
dollarveer <85345503+dollarveer@users.noreply.github.com>
2025-05-23 08:08:40 +0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -170,7 +170,7 @@ function deleteMessage(button, id) {
 				if (xhr.responseText.trim() === "success") {
 					const bubble = document.getElementById(`message-${messageId}`);
 				if(bubble) bubble.remove();
-				const content = encryptMessage("This message was deleted.", chatMessages[currentIdentity].chatPrint));
+				const content = encryptMessage("This message was deleted.", chatMessages[currentIdentity].chatPrint);
 				sendUpdatedMessage("delete", messageId, content);
 				}
 			} catch (e) {
